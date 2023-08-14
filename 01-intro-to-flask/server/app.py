6e242bc2b62025ddff900d01b46daee7d334de1a
stephenlambertfs <stephenolambert@gmail.com>
2023-08-14 10:22:58 -0600

    updated solution

--- 01-intro-to-flask/server/app.py
+++ 01-intro-to-flask/server/app.py
@@ -4,12 +4,31 @@
     # Request-Response Cycle
     # Web Servers and WSGI/Werkzeug
 
+#from werkzeug.wrappers import Request, Response
+
+
+# @Request.application
+# def application(request):
+#     print(f'This web server is running at {request.remote_addr}')
+#     return Response('A WSGI generated this response!')
+
+# if __name__ == '__main__':
+#     from werkzeug.serving import run_simple
+#     run_simple(
+#         hostname='localhost',
+#         port=5555,
+#         application=application
+#     )
+
 # 1. ✅ Navigate to `models.py`
 
 # 2. ✅ Set Up Imports
 	# `Flask` from `flask`
 	# `Migrate` from `flask_migrate`
 	# db and `Production` from `models`
+from flask import Flask, jsonify, make_response, request
+from flask_migrate import Migrate
+from models import db, Production
 
 # 3. ✅ Initialize the App
     # Add `app = Flask(__name__)`
@@ -20,7 +39,12 @@
     # Set the migrations with `migrate = Migrate(app, db)`
     
     # Finally, initialize the application with `db.init_app(app)`
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+migrate = Migrate(app,db)
+db.init_app(app)
  # 4. ✅ Migrate 
 	# `cd` into the `server` folder
 	
@@ -42,7 +66,9 @@
         # `@app.route('/')
         #  def index():
         #    return '<h1>Hello World!</h1>'`
-
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
 # 13. ✅ Run the server with `flask run` and verify your route in the browser at `http://localhost:5000/`
 
 # 14. ✅ Create a dynamic route
@@ -59,23 +85,29 @@
         # `make_response` will allow us to make a response object with the response body and status code
         # `jsonify` will convert our query into JSON
 
-    # `@app.route('/productions/<string:title>')
-    # def production(title):
-    #     production = Production.query.filter(Production.title == title).first()
-    #     production_response = {
-    #         "title":production.title,
-    #         "genre":production.genre,
-    #         "director": production.director
-    #         }
-    #     response = make_response(
-    #         jsonify(production_response),
-    #         200
-    #     )`    
+@app.route('/productions/<string:title>')
+def production(title):
+    production = Production.query.filter(Production.title == title).first()
+    production_response = {
+        "title":production.title,
+        "genre":production.genre,
+        "director": production.director
+        }
+    response = make_response(
+        jsonify(production_response),
+        200
+    )   
 
 # 16.✅ View the path and host with request context
-
+@app.route('/context')
+def context():
+    import ipdb; ipdb.set_trace()
+    return f'<h1>Path{request.path} Host:{request.host}</h1>'
 # 17.✅ Use the before_request request hook, what this hook does is up to you. You could hit a breakpoint, print something to server console or anything else you can think of.
-
+@app.before_request
+def runs_before():
+    current_user={"user_id":1, "username":"rose"}
+    print(current_user)
 # Note: If you'd like to run the application as a script instead of using `flask run`, uncomment the line below 
 # and run `python app.py`
 
