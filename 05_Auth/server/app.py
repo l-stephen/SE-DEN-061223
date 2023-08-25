3735cafce5400747dd1806f923a798d7ba1e1494
stephenlambertfs <stephenolambert@gmail.com>
2023-08-25 12:19:49 -0600

    updated solution

--- 05_Auth/server/app.py
+++ 05_Auth/server/app.py
@@ -6,7 +6,7 @@
 # Standard imports/boilerplate setup (We added session)
 
 # 7) import bcrypt here as well and wrap the app in bcrypt
-
+from flask_bcrypt import Bcrypt
 from flask import Flask, request, make_response, jsonify, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -20,6 +20,7 @@ migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
 CORS(app)
+bcrypt = Bcrypt(app)
 #1. ✅ python -c 'import os; print(os.urandom(16))'
 #Used to hash the session data
 app.secret_key = b'\\\xc2,\xc77\xb5\xf7\xb4\xed\xf2|o\x90U\xe7\xb8'
@@ -32,16 +33,56 @@ app.secret_key = b'\\\xc2,\xc77\xb5\xf7\xb4\xed\xf2|o\x90U\xe7\xb8'
         # 3.3.2 Use the name to query the user with a .filter
         # 3.3.3 If found set the user_id to the session hash
         # 3.3.4 convert the user to_dict and send a response back to the client 
+class Users(Resource):
+    def post(self):
+        form_json = request.get_json()
+
+        new_user = User(
+            name = form_json['name'],
+            password = form_json['password'],
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        print(session['user_id'])
+        return make_response(
+            new_user.to_dict(), 201)
+api.add_resource(Users, '/signup')
+
 
 # 3. Create a logout route now! set session to None
+class Logout(Resource):
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            print(session['user_id'])
+        return make_response({}, 204)
+api.add_resource(Logout, '/logout')
 
 # 4. Use @app.before_request, to run a function that checks if the session has the correct user, before every request
-
+@app.before_request
+def check_session():
+    print(session)
+    if session.get("user_id") is None:
+        session["user_id"] = None
+    else:
+        print("User is logged in")
+        print(session["user_id"])
 
 # 8) Create a route to sign in and authenticate the user
-
-
-
+class Login(Resource):
+    def post(self):
+        form_json = request.get_json()
+        name = form_json["name"]
+        password = form_json["password"]
+        user = User.query.filter_by(name=name).first()
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response("Invalid Credentials", 401)
+        
+api.add_resource(Login, '/signin')
 
 if __name__ == '__main__':
     app.run(port=5555)
--- 05_Auth/server/models.py
+++ 05_Auth/server/models.py
@@ -2,7 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 #5 )pip install flask_bcrypt and import bcrypt and wrap the app in bcrypt, import hybrid property
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt()
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+
 metadata = MetaData(naming_convention={
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -17,17 +21,23 @@ class User(db.Model,SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique = True)
-    password = db.Column(db.String)
+    _password_hash = db.Column(db.String)
     # 6) Add the password hash attribute
     
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     #6.1 Create a get method using hybrid property, and bcrypt
-
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
 
     #6.2 Create a setter method to set the password using bcrypt
-    
-
+    @password_hash.setter
+    def password(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
     #6.3 Create an authentication method to check the password using bcrypt
+    def authenticate(self,password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
 
